a365350ae14f20f70172252465a38f6d6646dbbf
brymoi <bryanmoises10@gmail.com>
2023-09-06 20:55:09 -0300

    implemented a config.h and a h-bridge lib

---
+++ src/config.h
@@ -0,0 +1,12 @@
+#define S1 36
+#define S2 34
+#define S3 39
+#define S4 35
+#define S5 13
+#define S6 4
+#define AIn1 14
+#define AIn2 27
+#define PWMA 33
+#define BIn1 26
+#define BIn2 25
+#define PWMB 32
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
+#include <H_bridge_TB6612.hpp>
+#include <config.h>
 
-#define S1 36
-#define S2 39
-#define S3 34
-#define S4 35
-#define S5 13
-#define S6 4
-#define left 18
-#define right 19
+#define channel1 1
+#define resolution_channel1 10
+#define channel2 3
+#define resolution_channel2 9
+
+Motor left = Motor(AIn1, AIn2, PWMA,  channel1, resolution_channel1);
+Motor right = Motor(BIn1, BIn2, PWMB,  channel2, resolution_channel2);
 
 void setup() {
   Serial.begin(9600);
@@ -16,39 +17,39 @@ void setup() {
   pinMode(S3, INPUT);
   pinMode(S4, INPUT);
   pinMode(S5, INPUT);
-  pinMode(left, OUTPUT);
-  pinMode(right, OUTPUT);
 }
 
 void loop() {
+  int IR1 = analogRead(S1);
   int IR2 = analogRead(S2);
   int IR3 = analogRead(S3);
   int IR4 = analogRead(S4);
+  int IR5 = analogRead(S5);
 
   Serial.print("S1: ");
-  Serial.print(analogRead(S1));
+  Serial.print(IR1);
   Serial.print(" | S2: ");
-  Serial.print(analogRead(S2));
+  Serial.print(IR2);
   Serial.print(" | S3: ");
-  Serial.print(analogRead(S3));
+  Serial.print(IR3);
   Serial.print(" | S4: ");
-  Serial.print(analogRead(S4));
+  Serial.print(IR4);
   Serial.print(" | S5: ");
-  Serial.println(analogRead(S5));
+  Serial.println(IR5);
 
   if(IR2 > 300){
     Serial.println("VIRA ESQUERDA");
-    digitalWrite(left, HIGH);
-    digitalWrite(right, LOW);
+    left.drive(1000);
+    right.drive(0);
   }
   if(IR4 > 300){
     Serial.println("VIRA DIREITA");
-    digitalWrite(left, LOW);
-    digitalWrite(right, HIGH);
+    right.drive(1000);
+    left.drive(1000);
   }
   if(IR3 > 1500){
     Serial.println("RETO");
-    digitalWrite(left, HIGH);
-    digitalWrite(right, HIGH);
+    left.drive(1000);
+    right.drive(1000);
   }
 }
