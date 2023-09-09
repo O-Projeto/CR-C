3bbfd49398f603f375c387e6f8107c1a66638a39
brymoi <bryanmoises10@gmail.com>
2023-09-09 10:41:29 -0300

    added bluetooth, QTR calibration, start and stop

--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,101 @@
 #include <Arduino.h>
 #include <H_bridge_TB6612.hpp>
 #include <config.h>
+#include <QTRSensors.h>
+#include <BluetoothSerial.h>
 
 #define channel1 1
-#define resolution_channel1 10
 #define channel2 3
-#define resolution_channel2 9
 
-int IR1min = 4095;
-int IR2min = 4095;
-int IR3min = 4095;
-int IR4min = 4095;
-int IR5min = 4095;
-int IR1max = 0;
-int IR2max = 0;
-int IR3max = 0;
-int IR4max = 0;
-int IR5max = 0;
-int IR1 = 0;
-int IR2 = 0;
-int IR3 = 0;
-int IR4 = 0;
-int IR5 = 0;
+QTRSensors qtr;
+const uint8_t SensorCount = 5;
+uint16_t sensorValues[SensorCount];
+BluetoothSerial SerialBT;
 
-Motor left = Motor(AIn1, AIn2, PWMA,  channel1, resolution_channel1);
-Motor right = Motor(BIn1, BIn2, PWMB,  channel2, resolution_channel1);
+Motor left = Motor(AIn1, AIn2, PWMA,  channel1, 10);
+Motor right = Motor(BIn1, BIn2, PWMB,  channel2, 10);
+
+double kP = 0;
+double kI = 0;
+double kD = 0;
+double kV = 500;
+bool start = false;
+
+void Calib(){
+  while(millis() < 10000){
+    qtr.calibrate();
+    delay(10);
+  }
+
+  Serial.println("Calibração Finalizada!");
+  SerialBT.println("Calibração Finalizada!");
+}
+
+void Iniciar(){
+}
+
+void PIDReceive(String ValorRecebido){
+    String stgkP = ValorRecebido.substring(2,9);
+    kP = stgkP.toDouble();
+    String stgkI = ValorRecebido.substring(10,17);
+    kI = stgkI.toDouble();
+    String stgkD = ValorRecebido.substring(18,25);
+    kD = stgkD.toDouble();
+    String stgkV = ValorRecebido.substring(26,29);
+    kV = stgkV.toDouble();
+    kV = kV * 10;
+
+    SerialBT.print(kP, 6);
+    SerialBT.print(" | ");
+    SerialBT.print(kI, 6);
+    SerialBT.print(" | ");
+    SerialBT.println(kD, 6);
+    SerialBT.print(" | ");
+    SerialBT.println(kV, 6);
+}
+
+void comunicationBT(){
+  if(SerialBT.available()){
+    String valorRecebido = SerialBT.readString();
+    SerialBT.println(valorRecebido);
+
+    if(valorRecebido == "Cliente Conectado!" || valorRecebido == "Cliente Desconectado!") SerialBT.print(valorRecebido);
+    else if(valorRecebido == "a") Calib();
+    else if(valorRecebido == "b") start = true;
+    else if(valorRecebido == "c") start = false;
+    else if(valorRecebido.substring(0, 1) == "d") PIDReceive(valorRecebido);
+    
+    SerialBT.println(start);
+  }
+}
 
 void setup() {
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){S1, S2, S3, S4, S5}, SensorCount);
+  qtr.setEmitterPin(2);
+
+  delay(500);
+
   Serial.begin(9600);
+  SerialBT.begin("Cerberus");
 
-  pinMode(S1, INPUT);
-  pinMode(S2, INPUT);
-  pinMode(S3, INPUT);
-  pinMode(S4, INPUT);
-  pinMode(S5, INPUT);
-
-  /*
-  while(millis() < 5000){
-    IR1 = analogRead(S1);
-    IR2 = analogRead(S2);
-    IR3 = analogRead(S3);
-    IR4 = analogRead(S4);
-    IR5 = analogRead(S5);
-    
-    if(IR1 > IR1max) IR1max = IR1;
-    if(IR2 > IR2max) IR2max = IR2;
-    if(IR3 > IR3max) IR3max = IR3;
-    if(IR4 > IR4max) IR4max = IR4;
-    if(IR5 > IR5max) IR5max = IR5;
-
-    if(IR1 < IR1min) IR1min = IR1;
-    if(IR2 < IR2min) IR2min = IR2;
-    if(IR3 < IR3min) IR3min = IR3;
-    if(IR4 < IR4min) IR4min = IR4;
-    if(IR5 < IR5min) IR5min = IR5;
-  }
-  */
+  delay(1000);
 }
 
 void loop() {
-  IR1 = analogRead(S1);
-  IR2 = analogRead(S2);
-  IR3 = analogRead(S3);
-  IR4 = analogRead(S4);
-  IR5 = analogRead(S5);
+  uint16_t position = qtr.readLineBlack(sensorValues);
 
-  Serial.print("S1: ");
-  Serial.print(IR1);
-  Serial.print(" | S2: ");
-  Serial.print(IR2);
-  Serial.print(" | S3: ");
-  Serial.print(IR3);
-  Serial.print(" | S4: ");
-  Serial.print(IR4);
-  Serial.print(" | S5: ");
-  Serial.println(IR5);
+  //CalibPrint();
 
-/*
-  if(IR2 > IR2min/2){
-    Serial.println("VIRA ESQUERDA");
-    left.drive(1000);
+  comunicationBT();
+  if (start == false){
+    left.drive(0);
     right.drive(0);
   }
-  if(IR4 > IR4min/2){
-    Serial.println("VIRA DIREITA");
-    right.drive(0);
-    left.drive(1000);
+  else if (start == true){
+    left.drive(kV);
+    right.drive(kV);
   }
-  if(IR3 > IR3min/2){
-    Serial.println("RETO");
-    left.drive(1000);
-    right.drive(1000);
-  }
-*/
-  left.drive(1000);
-  right.drive(1000);
+
   delay(10);
 }
